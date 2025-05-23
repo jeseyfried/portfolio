3d9512311236c4571c2dad41abb9455fc0c625c0
jeseyfried <jseyfried@unm.edu>
2025-05-23 07:19:58 -0600

    Update data.js

--- app-files/data.js
+++ app-files/data.js
@@ -76,7 +76,7 @@ var APP_DATA = {
           "yaw": -1.0435383471180888,
           "pitch": -0.15874789437096304,
           "title": "Two sample posters printed with the HP DesignJet T630",
-          "text": "This is an inkjet printer that prints on regular paper"
+          "text": "These are inkjet prints on regular paper"
         },
         {
           "yaw": 1.8294143652260466,
