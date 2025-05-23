47d2037f834d18506e7634254f501a03d6753677
jeseyfried <jseyfried@unm.edu>
2025-05-22 19:28:06 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,97 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-amaranth_tour",
+      "name": "Amaranth_Tour",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.7132512071801287,
+        "pitch": 0,
+        "fov": 1.3434819060782683
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.721476674079364,
+          "pitch": 0.09681577902485472,
+          "title": "CZUR Book Scanner",
+          "text": "Includes auto-mode that takes a scan of a book after each page turn."
+        },
+        {
+          "yaw": -3.0549012956728863,
+          "pitch": -0.007781207517082223,
+          "title": "Blue YETI Podcast Mic",
+          "text": "One of two podcast mics available in the Amaranth Lab. Both Mac workstations have Garage Band and Audacity for audio editing."
+        },
+        {
+          "yaw": -1.8943887001368864,
+          "pitch": -0.18901234909566078,
+          "title": "Blue YETI Podcast Mic",
+          "text": "One of two podcast mics available in the Amaranth Lab. Both Mac workstations have Garage Band and Audacity for audio editing."
+        },
+        {
+          "yaw": -2.8043664342401957,
+          "pitch": 0.010459113457411462,
+          "title": "HP DesignJet T630",
+          "text": "Poster printing up to 36\""
+        },
+        {
+          "yaw": -1.6068365194683452,
+          "pitch": -0.024354521395869355,
+          "title": "Revopoint POP 3 Plus 3D Scanner",
+          "text": "Includes turntable for easy scanning of objects."
+        },
+        {
+          "yaw": -0.5300519101914869,
+          "pitch": -0.05208012567302589,
+          "title": "Anycubic Kobra 3D Printer",
+          "text": "The Amaranth Lab offers several filament colors."
+        },
+        {
+          "yaw": 0.9488611482649567,
+          "pitch": -0.18841834685285797,
+          "title": "Samsung Video Conferencing Monitor",
+          "text": "Includes a soundbar for additional audio quality"
+        },
+        {
+          "yaw": -1.0435383471180888,
+          "pitch": -0.15874789437096304,
+          "title": "Two sample posters printed with the HP DesignJet T630",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.8294143652260466,
+          "pitch": -0.20409006224483583,
+          "title": "Sample 3D printed objects",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
